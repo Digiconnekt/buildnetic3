f0c7ad9ebe62985f9d26dc69a0df0f8012644f74
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-02-10 12:41:45 +0530

    fixed AOS responsive issue, pushing to check hero sec height

--- index.js
+++ index.js
@@ -28,6 +28,27 @@ for (let i = 0; i < menuItem.length; i++) {
     }
   }
 }
+
+let servicesPath = location.pathname.substr(
+  0,
+  location.pathname.lastIndexOf("/")
+);
+
+if (location.pathname === "/") {
+  menuItem[0].className = "nav-link active";
+} else if (
+  location.pathname === "/about-us.html" ||
+  location.pathname === "/careers.html" ||
+  location.pathname === "/faq.html"
+) {
+  menuItem[1].className = "nav-link dropdown-toggle active";
+} else if (
+  location.pathname === "/services.html" ||
+  servicesPath === "/services"
+) {
+  menuItem[5].className = "nav-link dropdown-toggle active";
+}
+
 // active class for header end
 
 // footerActive class for footer start
