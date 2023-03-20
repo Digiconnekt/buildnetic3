de81f2e79f3867ec8f8b0412434d4c9aa92e27e3
Surender Aggarwal <surender.singal@gmail.com>
2023-03-20 11:42:59 +0000

    20March Surender

---
+++ privacy-policy.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Buildnetic</title>
+
+  <!-- favicon link -->
+  <link rel="shortcut icon" href="../images/buildnetic-fav.ico" type="image/x-icon" />
+
+  <!-- bootstrap 5 CSS cdn link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+
+  <!-- custom CSS link -->
+  <link rel="stylesheet" href="../css/index.css" />
+
+  <!-- custom CSS link for SERVICES MAIN page-->
+  <link rel="stylesheet" href="../css/single-service.css" />
+
+  <!-- fontawesome link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- internal css -->
+  <style>
+    .services-we-offer-section .card {
+      /* min-height: 270px; */
+    }
+
+    @media (max-width: 1440px) {
+      .services-we-offer-section .card {
+        min-height: 300px;
+      }
+    }
+
+    @media (max-width: 1200px) {
+      .services-we-offer-section .card {
+        min-height: 320px;
+      }
+    }
+
+    @media (max-width: 991px) {
+      .services-we-offer-section .card {
+        min-height: 370px;
+      }
+    }
+
+    @media (max-width: 767px) {
+      .services-we-offer-section .card {
+        min-height: auto;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <!-- header section start -->
+  <?php include '../partials/menu.php' ?>
+  <!-- header section end -->
+
+  <div class="single-service-page">
+    <!-- title section start -->
+    <div class="testing-banner title-section container-fuild mt-3">
+      <div class="px-4 py-5 text-center">
+
+        <h1 class="display-5 fw-bold mb-3">Privacy Policy</h1>
+      </div>
+    </div>
+    <!-- title section end -->
+
+    <!-- development section start -->
+    <div class="development-section container my-5">
+      <div class="row">
+
+        <div class="col-12">
+
+          <h3>Privacy Policy</h3>
+          <p>At Buildnetic, we are committed to protecting the privacy and security of our customers and visitors to our website. This Privacy Policy explains how we collect, use, and disclose information about you when you use our website.</p>
+
+
+          <p>This Privacy Policy governs the way in which BUILDNETIC uses and divulges information related to their users. This information is collected on http://www.buildnetic.com/ (website). The Privacy Policy is applicable to the website and all its products and services.</p>
+
+          <h3> Personal and non-personal identification information</h3>
+          <p>When you visit our website, we may collect certain information automatically from your device, such as your IP address, browser type, operating system, and other technical information. We may also collect information about your use of our website, such as pages visited and time spent on each page.</p>
+
+          <p>If you contact us via email, we may collect personal information such as your name, email address, and any other information you choose to provide.</p>
+
+          <p>We may also collect information through the use of cookies or other tracking technologies. These technologies may be used to collect information about your use of our website and to deliver personalized content and advertising.</p>
+
+          <h3>How we use the collected information?</h3>
+          <p>We may use the information we collect for a variety of purposes, including:</p>
+          <ul>
+            <li>To operate and improve our website and services</li>
+            <li>To communicate with you about your account or any inquiries you have made</li>
+            <li>To send you marketing communications and offers that we believe may be of interest to you</li>
+            <li>To comply with legal obligations or respond to legal requests</li>
+          </ul>
+
+          <p>You can send us a mail at sales@buildnetic.com if you wish us to delete or change your personal identification information.
+          </p>
+
+          <h3>Web Cookies</h3>
+          <p>The web browser stores the cookies on the users’ hard drive to track information about them. The purpose of using cookies is to improve user experience. In case the users do not want their activity to be tracked via cookies, they can refuse cookies (in which case some features of the website may not function properly).</p>
+          <h3>We protect your information</h3>
+          <p>We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure. However, no security measures can provide absolute protection, and we cannot guarantee the security of your information.</p>
+          <h3>Your choices</h3>
+          <p>You may opt out of receiving marketing communications from us at any time by clicking the unsubscribe link in our emails.</p>
+          <p>You may also disable cookies in your browser settings, although this may impact your ability to use some features of our website.</p>
+          <h3>Third Party Websites</h3>
+          <p>If there are websites that advertise our services or have a link to our website, browsing and interaction with any such website will depend on their terms and policies and completely beyond our control.</p>
+          <h3>Privacy Policy Update</h3>
+          <p>BUILDNETIC has the right to update the privacy policy at any time to reflect changes in our practices or legal requirements. We will provide notice of any material changes by posting the updated policy on our website.</p>
+          <h3>Acceptance of this Privacy Policy</h3>
+          <p>By using this website, you agree to this privacy policy. In case you do not agree with this privacy policy, you are suggested to refrain from the use of our website.
+            If you have any questions or concerns about our Privacy Policy or our practices, you can drop us an email at sales@buildnetic.com.</p>
+
+
+
+        </div>
+      </div>
+    </div>
+    <!-- development section end -->
+
+
+
+
+
+
+
+  </div>
+
+  <!-- footer section start -->
+  <?php include '../partials/footer.php' ?>
